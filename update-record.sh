62c07a3971a487af74c2bc2db3be67b6fca1238f
aaaa777(piserver) <aaaa777@users.noreply.github.com>
2021-12-25 21:09:51 +0900

    fix code

--- update-record.sh
+++ update-record.sh
@@ -4,16 +4,16 @@
 #USERNAME, PASSWORD, TENANTID, DOMAIN
 
 . ./api.conf
-
+echo $USERNAME
 
 #read last global ip from temporary file
 
-_last_global_ip=cat current-global-ip.tmp
+_last_global_ip=`cat current-global-ip.tmp`
 
 
 #GET global ip
 
-_now_global_ip=eval $IP_CHECKER
+_now_global_ip=`eval "$IP_CHECKER"`
 
 
 #compare now/last global ip
@@ -31,16 +31,15 @@ fi
 echo "old global ip: $_last_global_ip, new global ip: $_now_global_ip"
 echo "Getting access token..."
 
-_curl=`cat << EOS
-curl -X POST \
+#_curl=cat << EOS
+_access_token=`curl -sL -X POST \
 -H "Accept: application/json" \
--d '{"auth":{"passwordCredentials":{"username":"$USERNAME","password":"$PASSWORD"},"tenantId":"$TENANTID"}}' \
-$CONOHA_API_ID_ENDPOINT/tokens |
-jq -r '.access.token.id'
-EOS
-`
+-d "{\"auth\":{\"passwordCredentials\":{\"username\":\"$USERNAME\",\"password\":\"$PASSWORD\"},\"tenantId\":\"$TENANTID\"}}" \
+"$CONOHA_API_ID_ENDPOINT/tokens" \
+ | jq -r '.access.token.id'`
+#EOS
 
-_acccess_token=$($_curl)
+#_acccess_token=$($_curl)
 
 
 echo "access token responce: $_access_token"
@@ -48,56 +47,51 @@ echo "GET DNS A record forwarding from $DOMAIN ..."
 
 #GET DNS A record with _access_token
 
-_curl=`cat <<EOS
-curl -X GET \
--H "Accept: application/json" \
--H "Content-Type: application/json" \
--H "X-Auth-Token: 39be9f8d53044388b7f2e867eba8b140" \
-$CONOHA_API_DNS_ENDPOINT/v1/domains?name=$DOMAIN. |
-jq -r '.domains[] | select(.name == "$DOMAIN.")'
-EOS
-`
-
-_domain_id=$($_curl)
-
-
-#get record id
-echo "getting record id..."
-
-_curl=`cat << EOS
-curl -X GET \
+_domain_id=`curl -X GET -sL \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
 -H "X-Auth-Token: $_access_token" \
-$CONOHA_API_DNS_ENDPOINT/v1/domains/$_domain_id/records |
-jq -r '.records[] | select(.type == "A")'
-EOS
-`
+"$CONOHA_API_DNS_ENDPOINT/v1/domains?name=$DOMAIN." | \
+jq -r ".domains | .[] | select(.name == \"$DOMAIN.\") | .id"`
+#EOS
 
-_record_id=$($_curl)
+#_domain_id=$($_curl)
 
+echo "_domain_id: $_domain_id"
+#get record id
+echo "getting record id..."
+
+_record_id=`curl -X GET -sL \
+-H "Accept: application/json" \
+-H "Content-Type: application/json" \
+-H "X-Auth-Token: $_access_token" \
+$CONOHA_API_DNS_ENDPOINT/v1/domains/$_domain_id/records \
+ | jq -r '.records | .[] | select(.type == "A") | .id'`
+
+
+#_record_id=$($_curl)
+echo "record_id: $_record_id"
 
 #update A record
 echo "updating A record..."
 
-_curl=`cat << EOS
-curl $CONOHA_API_DNS_ENDPOINT/v1/domains/$_domain_id/records/$_record_id
+_response=`curl -sL \
+ $CONOHA_API_DNS_ENDPOINT/v1/domains/$_domain_id/records/$_record_id \
 -X PUT \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
 -H "X-Auth-Token: $_access_token" \
--d
-'{
-  "name": "$DOMAIN",
-  "type": "A",
-  "data": "$_now_global_ip"
-}'
-EOS
-`
+-d "{
+  \"name\": \"$DOMAIN.\",
+  \"type\": \"A\",
+  \"data\": \"$_now_global_ip\"
+}"`
+#EOS
 
+echo $_response
 #save global ip temporary
 
-_now_global_ip > current-global-ip.tmp
+echo "$_now_global_ip" > current-global-ip.tmp
 
 echo "global ip address updated successfuly!"
 echo "updated: $_last_global_ip -> $_now_global_ip"
